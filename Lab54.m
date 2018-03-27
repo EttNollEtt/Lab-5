b7076dae13f4683dfc77bebb6197b0b5454c1ea1
0KevinK <33936552+0KevinK@users.noreply.github.com>
2018-03-27 21:37:33 +0200

    Update Lab54.m

--- Lab54.m
+++ Lab54.m
@@ -2,7 +2,7 @@ clear all
 close all 
 clc
 
-%Givna värden
+%Givna vÃ¤rden
 L1 = 1; 
 L2 = 1;
 L3 = 1;
@@ -25,45 +25,45 @@ uV = [u1; u2; u3];
 f1 = L1*cos(u1) + L2*cos(u2) + L3*cos(u3) - a;
 f2 = L1*sin(u1) + L2*sin(u2) + L3*sin(u3);
 f3 = m2*tan(u1) - (m1+m2)*tan(u2) + m1*tan(u3);
-f = [f1; f2; f3]; % Vektor med funktionsvärderna
+f = [f1; f2; f3]; % Vektor med funktionsvÃ¤rderna
 
-% Newtons metod för ickelinjära ekvationssystem
-% Jacobian fås som [df1/du1 df1/du2 df1/du3 ; df2/du1 ....
+% Newtons metod fÃ¶r ickelinjÃ¤ra ekvationssystem
+% Jacobian fÃ¥s som [df1/du1 df1/du2 df1/du3 ; df2/du1 ....
 
 iter = 0; % Iteration
-dxnorm = 1;
+dfnorm = 1;
 disp('   Iteration    u1       u2        u3      Kvadreringhastighet')
 while norm(f) > 1e-10 && iter < 10 
-    % Sätter stoppkriterium för loopen, vi vill att alla element i f
-    % dvs ekvationerna, ska vara noll. Därför norm(f) tillräckligt nära
+    % SÃ¤tter stoppkriterium fÃ¶r loopen, vi vill att alla element i f
+    % dvs ekvationerna, ska vara noll. DÃ¤rfÃ¶r norm(f) tillrÃ¤ckligt nÃ¤ra
     % noll.
-    dxnormprim = dxnorm;
+    dfnormprim = dfnorm;
     iter = iter + 1; 
     J = [-L1*sin(u1) -L2*sin(u2) -L3*sin(u3);
         L1*cos(u1) L2*cos(u2) L3*cos(u3);
         m2/(cos(u1)^2) -(m1+m2)/(cos(u2)^2) m1/(cos(u3)^2)]; 
-    df = J\f;       %derivatan
+    df = J\f;       %Derivatan
     uV = uV - df;   %Precis som Newton metod i en variabel
-    u1 = uV(1); % Stoppar in värden
+    u1 = uV(1); % Stoppar in vÃ¤rden
     u2 = uV(2); 
     u3 = uV(3);  
-    f1 = L1*cos(u1) + L2*cos(u2) + L3*cos(u3) - a; % Uppdatera funktionsvärde
+    f1 = L1*cos(u1) + L2*cos(u2) + L3*cos(u3) - a; % Uppdatera funktioner
     f2 = L1*sin(u1) + L2*sin(u2) + L3*sin(u3); 
     f3 = m2*tan(u1) - (m1+m2)*tan(u2) + m1*tan(u3); 
     f = [f1; f2; f3]; 
-    dxnorm = norm(df,inf);  % Normerad derivata
-    k = dxnorm/dxnormprim^2; % Derivata/gammla derivatan i kvadrat    
+    dfnorm = norm(df);
+    k = dfnorm/dfnormprim^2; % Derivata/gammla derivatan i kvadrat    
     disp([iter u1 u2 u3 k])
 end
 
 
-% Plottar snöret
-%Börjar med koordinater för A o B
+%Plottar snÃ¶ret
+%BÃ¶rjar med koordinater fÃ¶r A o B
 Ax = 0; 
 Ay = 0; 
 Bx = a; 
 By = 0; 
-%Koordinater för m1 o m2
+%Koordinater fÃ¶r m1 o m2
 m2x = Bx - L3*cos(u3); 
 m2y = By + L3*sin(u3); 
 m1x = Ax + L1*cos(u1); 
